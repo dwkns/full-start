411408c4eadc07ae01784dda4d3a64aec55ec694
dwkns <dwkns@me.com>
2021-12-06 09:54:18 +0000

    Update nearly everything

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,8 @@
 import Alpine from 'alpinejs'
+import Cookies from "js-cookie";
 console.log('JS is working');
+
+window.Cookies = Cookies
 
 window.addEventListener('DOMContentLoaded', () => {
   window.Alpine = Alpine // Add Alpine to the Global Scope.
